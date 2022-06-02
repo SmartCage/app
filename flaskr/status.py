57b167dd1bf6c9f00de5d589a40e20206a4ab43c
Daniel7398 <62849456+Daniel7398@users.noreply.github.com>
2022-06-02 11:32:45 +0300

    Added MQTT and db.
    Added MQTT and db.

---
+++ flaskr/status.py
@@ -0,0 +1,21 @@
+from db import get_db
+
+def get_status():
+    food_data = get_db().execute(
+        'SELECT id, timestamp, type, quantity'
+        ' FROM food'
+        ' ORDER BY timestamp DESC'
+    ).fetchone()
+
+    if food_data is None:
+        return {'status': 'The food isn\'t set'}
+
+    return {
+        'food': {
+                'type': food_data['type'],
+                'quantity': food_data['quantity'],
+                'timestamp': food_data['timestamp']
+        }
+
+    }
+    
